72939fa52ac0b362ab715a142b872fbd899b614b
Sam <tritoke@protonmail.com>
2022-02-06 01:39:32 +0000

    Initial commit of go code.

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module go_server
 
 go 1.17
+
+require github.com/gocarina/gocsv v0.0.0-20211203214250-4735fba0c1d9 // indirect
--- go/main_server.go
+++ go/main_server.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"github.com/gocarina/gocsv"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//ch := make(chan PiDataValue)
+
+	file, err := os.OpenFile("data.csv", os.O_RDWR | os.O_CREATE, os.ModePerm)
+	if err != nil {
+			fmt.Println(err)
+			return
+	}
+	defer file.Close()
+
+	data := []PiDataValue{}
+
+	err = gocsv.UnmarshalFile(file, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data)
+
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		switch r.Method {
+			case "POST":
+				body, err := ioutil.ReadAll(r.Body)
+
+				if err != nil {
+					fmt.Printf("Error reading body: %v", err)
+					http.Error(w, `{"message": "Couldn't read body."}`, http.StatusBadRequest)
+					return
+				}
+
+				var x PiDataValue
+				err = json.Unmarshal(body, &x)
+				if err != nil {
+					fmt.Printf("Error parsing body: %v", err)
+					http.Error(w, `{"message": "Couldn't parse body."}`, http.StatusBadRequest)
+					return
+				}
+
+				err = gocsv.MarshalWithoutHeaders([]PiDataValue{x}, file)
+				if err != nil {
+					fmt.Printf("Error marshalling data: %v", err)
+					http.Error(w, `{"message": "Failed to write data."}`, http.StatusInternalServerError)
+					return
+				}
+
+				data = append(data, x)
+
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Data received successfully."}`))
+			case "GET":
+				d, err := json.Marshal(map[string][]PiDataValue { "data": data })
+				if err != nil {
+					fmt.Printf("Error serialising data: %v", err)
+					http.Error(w, `{"message": "Failed to serialise data."}`, http.StatusInternalServerError)
+					return
+				}
+
+        w.WriteHeader(http.StatusOK)
+        w.Write(d)
+			default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte(`{"message": "not found"}`))
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type PiDataValue struct {
+	Uid int `json:"uid" csv:"uid"`
+	Shakiness int `json:"shakiness" csv:"shakiness"`
+	Concern int `json:"concern" csv:"concern"`
+	Stddev float64 `json:"stddev" csv:"stddev"`
+	Day int `json:"day" csv:"day"`
+	Month int `json:"month" csv:"month"`
+	Year int `json:"year" csv:"year"`
 }
